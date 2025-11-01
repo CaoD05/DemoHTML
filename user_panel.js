28ce3ba3ae08f83ce6677cb5d6c2acb9e3ba4176
CaoD05 <neverwintop@gmail.com>
2025-11-01 11:34:13 +0700

    Update user_panel.js

--- user_panel.js
+++ user_panel.js
@@ -11,27 +11,27 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       title: "Bleach",
       category: "Supernatural / Shonen",
-      img: "https://upload.wikimedia.org/wikipedia/en/7/72/Bleach_cover_01.jpg"
+      img: "https://upload.wikimedia.org/wikipedia/vi/5/53/Bleach_cover_01.jpg"
     },
     {
       title: "One Piece",
       category: "Adventure / Comedy",
-      img: "https://upload.wikimedia.org/wikipedia/en/2/2c/OnePiece01Cover.jpg"
+      img: "https://upload.wikimedia.org/wikipedia/vi/9/90/One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg"
     },
     {
       title: "Attack on Titan",
       category: "Action / Dark Fantasy",
-      img: "https://upload.wikimedia.org/wikipedia/en/0/0c/Attack_on_Titan_volume_1_cover.jpg"
+      img: "https://upload.wikimedia.org/wikipedia/vi/8/8d/%C4%90%E1%BA%A1i_chi%E1%BA%BFn_Titan_manga_t%E1%BA%ADp_1_ti%E1%BA%BFng_Vi%E1%BB%87t.png"
     },
     {
       title: "Demon Slayer",
       category: "Action / Historical",
-      img: "https://upload.wikimedia.org/wikipedia/en/f/f9/Kimetsu_no_Yaiba_Volume_1.jpg"
+      img: "https://upload.wikimedia.org/wikipedia/vi/0/09/Demon_Slayer_-_Kimetsu_no_Yaiba%2C_volume_1.jpg"
     },
     {
       title: "My Hero Academia",
       category: "Superhero / Action",
-      img: "https://upload.wikimedia.org/wikipedia/en/0/03/My_Hero_Academia_Volume_1.png"
+      img: "https://upload.wikimedia.org/wikipedia/vi/5/5a/Boku_no_Hero_Academia_Volume_1.png"
     }
   ];
 
